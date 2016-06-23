55d320358253eb158740ec7a21ffcc84d9748f64
aspll <anabelle.spallek@gmail.com>
2016-06-23 18:43:25 +0200

    Added per PhpStorm 2

--- ha-15.js
+++ ha-15.js
@@ -9,3 +9,12 @@ $("#tp2").tooltipster( {
 $("#tp3").tooltipster( {
     content: $("#tp3-c").detach()
 });
+
+
+$("#tp4").tooltipster( {
+    content: $("#tp4-c").detach()
+});
+
+$("#tp5").tooltipster( {
+    content: $("#tp5-c").detach()
+});
