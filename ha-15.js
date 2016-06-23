a64858ea5c8078e782a480c0d5b8f0f3d6f06afe
aspll <anabelle.spallek@gmail.com>
2016-06-23 18:06:01 +0200

    Added per PhpStorm

--- ha-15.js
+++ ha-15.js
@@ -1,3 +1,11 @@
-/**
- * Created by asp on 23.06.16.
- */
+$("#tp1").tooltipster( {
+    content: $("#tp1-c").detach()
+});
+
+$("#tp2").tooltipster( {
+    content: $("#tp2-c").detach()
+});
+
+$("#tp3").tooltipster( {
+    content: $("#tp3-c").detach()
+});
